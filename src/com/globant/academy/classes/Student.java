035b3621181e8d0881321c4ab4846463e00fbab8
JherCas <jhercas18@hotmail.com>
2021-10-12 17:27:20 -0500

    Final

--- src/com/globant/academy/classes/Student.java
+++ src/com/globant/academy/classes/Student.java
@@ -17,19 +17,8 @@ public class Student {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public float getAge() {
         return age;
     }
 
-    public void setAge(float age) {
-        this.age = age;
-    }
-
-    public UUID getId() {
-        return uuid;
-    }
 }
